e6388dcc0d10983dbad4baf5d042d7f7f8b951e1
Rohit B Iyer <rohitbiyer@rohits-air.wireless.rit.edu>
2017-02-19 21:16:07 -0500

    react-skeleton

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var ListA = require('./components/List.jsx');
+
+ReactDom.render(<ListA />, document.getElementById('ingredients'));
